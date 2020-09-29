743c65b20ebe24e90af0386bd1ce13a1f3864ae9
Miroslav Talasek <miroslav.talasek@seznam.cz>
2020-09-29 23:24:52 +0200

    scrolable

--- hello_world2/lib/widgets/transaction_list.dart
+++ hello_world2/lib/widgets/transaction_list.dart
@@ -4,53 +4,54 @@ import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
- 
   final List<Transaction> transactions;
 
   TransactionList(this.transactions);
   @override
   Widget build(BuildContext context) {
-    return Column(
-              children: transactions.map((e) {
-            return Card(
-                elevation: 3,
-                child: Row(
-                  children: [
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          border: Border.all(
+    return Container(
+      height: 300,
+      child: ListView.builder(itemBuilder: (ctx,index){
+        return Card(
+              elevation: 3,
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    child: Text(
+                      '\$${transactions[index].amount}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
                         color: Colors.purple,
-                        width: 2,
-                      )),
-                      child: Text(
-                        '\$${e.amount}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
                       ),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          e.title,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 16),
-                        ),
-                        Text(
-                          DateFormat('dd.MM.yyyy').format(e.date),
-                          style: TextStyle(color: Colors.grey),
-                        )
-                      ],
-                    )
-                  ],
-                ));
-          }).toList(),
-      );
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        transactions[index].title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      Text(
+                        DateFormat('dd.MM.yyyy').format(transactions[index].date),
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ));
+      },
+      itemCount:transactions.length ,
+      ),
+    );
   }
 }
