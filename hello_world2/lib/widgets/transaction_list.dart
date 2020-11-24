46327ef7ec5622652d75ee9f4e9a3fa8e611771e
Miroslav Talasek <miroslav.talasek@seznam.cz>
2020-11-24 23:24:51 +0100

    dynamic size

--- hello_world2/lib/widgets/transaction_list.dart
+++ hello_world2/lib/widgets/transaction_list.dart
@@ -10,51 +10,49 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.transactions, this.delTrans);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: transactions.isEmpty
-          ? Column(children: [
-              Text("No transactions yet",
-                  style: Theme.of(context).textTheme.title),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                  height: 200,
-                  child: Image.asset('assets/images/waiting.png',
-                      fit: BoxFit.cover))
-            ])
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  elevation: 5,
-                  child: ListTile(
-                    leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: EdgeInsets.all(6),
-                          child: FittedBox(
-                              child: Text('\$${transactions[index].amount}')),
-                        )),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(
-                      DateFormat('dd.MM.yyyy').format(transactions[index].date),
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () {
-                        delTrans(transactions[index].id);
-                      },
-                    ),
+    return transactions.isEmpty
+        ? Column(children: [
+            Text("No transactions yet",
+                style: Theme.of(context).textTheme.title),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+                height: 200,
+                child:
+                    Image.asset('assets/images/waiting.png', fit: BoxFit.cover))
+          ])
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                elevation: 5,
+                child: ListTile(
+                  leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                            child: Text('\$${transactions[index].amount}')),
+                      )),
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.title,
                   ),
-                );
-                /*return Card(
+                  subtitle: Text(
+                    DateFormat('dd.MM.yyyy').format(transactions[index].date),
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () {
+                      delTrans(transactions[index].id);
+                    },
+                  ),
+                ),
+              );
+              /*return Card(
                     elevation: 3,
                     child: Row(
                       children: [
@@ -92,9 +90,8 @@ class TransactionList extends StatelessWidget {
                         )
                       ],
                     ));*/
-              },
-              itemCount: transactions.length,
-            ),
-    );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
